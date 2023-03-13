63b85cbb48340bb1747d65ad042790eec92bb21d
André Almeida <almeida.ae@outlook.com>
2023-03-13 14:45:43 -0300

    inicializando o projeto

---
+++ relogio.py
@@ -0,0 +1,21 @@
+from tkinter import *
+import tkinter
+
+#---------Cores------------
+cor1 = "#3d3d3d" #Black
+cor2 = "#fafcff" #White
+cor3 = "#21c25c" #Green
+cor4 = "#eb463d" #Red
+cor5 = "#dedcdc" #Cinza
+cor6 = "#3080f0" #Blue
+
+fundo = cor1
+cor = cor2
+
+janela=Tk()
+janela.title("")
+janela.geometry("440x180")
+janela.resizable(width=FALSE, height=FALSE)
+janela.configure(bg=cor1)
+
+janela.mainloop()
