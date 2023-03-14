a2ec5e898c75d398696a1aa9ac1ebe910bcc0d4c
André Almeida <almeida.ae@outlook.com>
2023-03-14 11:26:53 -0300

    configurando a janela

--- relogio.py
+++ relogio.py
@@ -1,5 +1,8 @@
 from tkinter import *
 import tkinter
+from datetime import datetime
+
+
 
 #---------Cores------------
 cor1 = "#3d3d3d" #Black
@@ -10,7 +13,7 @@ cor5 = "#dedcdc" #Cinza
 cor6 = "#3080f0" #Blue
 
 fundo = cor1
-cor = cor2
+cor = cor4
 
 janela=Tk()
 janela.title("")
@@ -18,4 +21,26 @@ janela.geometry("440x180")
 janela.resizable(width=FALSE, height=FALSE)
 janela.configure(bg=cor1)
 
+def relogio():
+    tempo = datetime.now()
+    hora = tempo.strftime("%H:%M:%S")
+    dia_semana=tempo.strftime("%A")
+    dia=tempo.day
+    mes=tempo.strftime("%B")
+    ano=tempo.strftime("%Y")
+
+    l1.config(text=hora)
+    l1.after(200, relogio)
+    l2.config(text=dia_semana + " " + str(dia) + "/" + str(mes) + "/" + str(ano))
+
+
+l1 = Label(janela,text="", font=("Arial 80"), bg=fundo, fg=cor)
+l1.grid(row=0, column=0, sticky=NW, padx=5)
+
+l2 = Label(janela,text="", font=("arial 20"), bg=fundo, fg=cor)
+l2.grid(row=1, column=0, sticky=NW, padx=5)
+
+relogio()
+
 janela.mainloop()
+
